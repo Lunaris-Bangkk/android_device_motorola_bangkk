cc38fff0495a6048cd8fa53530f9800e8b92ea6b
ARYANGODxDOfficial <aryansanjay.080805@gmail.com>
2026-07-08 06:28:50 +0000

    bangkk: Bringup For @Evolution-X

--- lineage_bangkk.mk
+++ lineage_bangkk.mk
@@ -26,3 +26,9 @@ PRODUCT_BUILD_PROP_OVERRIDES += \
     BuildDesc="bangkk_g-user 15 V1TCS35H.88-16-4 d201a-da14f release-keys MV-301" \
     BuildFingerprint=motorola/bangkk_g/bangkk:15/V1TCS35H.88-16-4/d201a-da14f:user/release-keys \
     DeviceProduct=bangkk_g
+
+# Flags
+WITH_GMS := true
+TARGET_HAS_UDFPS := true
+BUILD_BCR := true
+TARGET_ENABLE_BLUR := false
