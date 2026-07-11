93b4617e1c1bdcaf54dc6fe5bcf0f35466449baa
ARYANGODxDOfficial <aryansanjay.080805@gmail.com>
2026-07-11 19:24:35 +0000

    bangkk: Add unofficial OTA and move ro.surface_flinger.set_idle_timer_ms to common dt
    Signed-off-by: ARYANGODxDOfficial <aryansanjay.080805@gmail.com>

--- device.mk
+++ device.mk
@@ -28,7 +28,8 @@ PRODUCT_PACKAGES += \
     ProductFrameworksResDevice \
     SettingsResDevice \
     SystemUIResDevice \
-    WifiResDevice
+    WifiResDevice \
+    InfinityUpdater
 
 # Audio
 PRODUCT_COPY_FILES += \
