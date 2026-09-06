ae453863c6fabef7eeaaf1a2ba8fc975046730c7
ARYANGODxDOfficial <aryansanjay.080805@gmail.com>
2026-09-06 14:28:40 +0000

    bangkk: Bringup For @RisingOS-Revived
    Signed-off-by: ARYANGODxDOfficial <aryansanjay.080805@gmail.com>

--- lineage_bangkk.mk
+++ lineage_bangkk.mk
@@ -26,3 +26,28 @@ PRODUCT_BUILD_PROP_OVERRIDES += \
     BuildDesc="bangkk_g-user 15 V1TCS35H.88-16-8 2762f-8b2b3d release-keys MV-301" \
     BuildFingerprint=motorola/bangkk_g/bangkk:15/V1TCS35H.88-16-8/2762f-8b2b3d:user/release-keys \
     DeviceProduct=bangkk_g
+
+# Lunch banner maintainer variable
+RISING_MAINTAINER="Aryan S"
+
+# Chipset/Maintainer properties (ro.rising.chipset/ro.rising.maintainer) 
+# Set RISING_MAINTAINER for version control 
+# (Optional if builder is setting properties via init_<device>.cpp)
+PRODUCT_BUILD_PROP_OVERRIDES += \
+    RisingChipset="Snapdragon 695" \
+    RisingMaintainer="Aryan S"
+
+RISING_MAINTAINER := Aryan S
+
+# Disable/enable blur support, false by default
+TARGET_ENABLE_BLUR := true
+
+# Whether to ship aperture camera, false by default
+PRODUCT_NO_CAMERA := true
+
+# CORE build flags
+WITH_GMS := true
+TARGET_USES_PICO_GAPPS := true
+
+# UDFPS
+TARGET_HAS_UDFPS := true
